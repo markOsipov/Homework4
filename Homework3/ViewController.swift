08d9b15ea64a03fca69a0a8b1789c2146efd8c92
Mark Osipov <m.osipov@cardsmobile.ru>
2021-07-14 23:21:51 +0300

    Initial Commit

---
+++ Homework3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Mark Osipov on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
